f2ce2c15c3fb2b32ea5d9b13df76bee2948f543a
PurrplingCat <git@purrplingcat.com>
2020-07-23 19:17:07 +0200

    Improved date parser

--- Utils.cs
+++ Utils.cs
@@ -7,9 +7,36 @@ namespace PurrplingCore
 {
     class Utils
     {
+        private static bool TryParseRelativeDate(string strDate, out SDate date)
+        {
+            switch (strDate)
+            {
+                case "today":
+                    date = SDate.Now();
+                    break;
+                case "yesterday":
+                    date = SDate.Now().AddDays(-1);
+                    break;
+                case "tomorrow":
+                    date = SDate.Now().AddDays(1);
+                    break;
+                default:
+                    date = null;
+                    break;
+            }
+
+            return date != null;
+        }
+
         public static SDate ParseDate(string strDate)
         {
             string[] s = strDate.Split(' ');
+
+            if (TryParseRelativeDate(strDate, out SDate date))
+                return date;
+
+            if (s.Length == 2)
+                return new SDate(Convert.ToInt32(s[0]), s[1]);
 
             if (s.Length != 3 || !s[2].StartsWith("Y"))
                 throw new ParseDateException("Input date string has wrong format!");
